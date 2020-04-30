03afad09a2dae949a887613284648ac9a3ffa6d8
Tony Bathgate <tbathgate@vendasta.com>
2020-04-29 18:05:14 -0600

    [WP-623] Add description documenting the storage class and how the data is structured
    branch:WP-623/add-profile-fields-for-real
    [WP-623](https://vendasta.jira.com/browse/WP-623)

--- includes/class-data-storage.php
+++ includes/class-data-storage.php
@@ -1,75 +1,89 @@
 <?php
-
+/**
+ * This DataStorage Class is meant to fetch and access the Business Profile Data
+ *
+ * It is intended to operate as a Singleton to limit operations on the DataBase.
+ * It is Read-Only - the data is written to the WordPress Options table by the hosting
+ * platform.
+ *
+ * This class provides a `get` method that returns the value corresponding to the key. If there was
+ * no data saved in the WordPress Option table, it will return default dummy data.
+ *
+{
+    "company_name": "Your Company",
+    "address": "6 W 29th St",
+    "city": "New York",
+    "state": "NY",
+    "zip": "10001",
+    "country": "US",
+    "work_number": [
+        "234-567-8901",
+        "(432) 343-2132",
+    ],
+    "toll_free_number": "800-234-0987",
+    "description": "This is testing the loooonoooong description",
+    "short_description": "your short description",
+    "contact_email": null,
+    "contact_first_name": "Jane",
+    "contact_last_name": "Doe",
+    "cell_number": "213-281-7668",
+    "images": {
+        "logo": "https://media-prod.apigateway.co/image/get/01ce062e-7f44-4dea-b0b1-f757334a2a5f",
+        "primary": "https://media-prod.apigateway.co/image/get/9eda55a6-37a3-422d-a6a4-af91f51a46b9"
+    },
+    "timezone": "America/New_York",
+    "foursquare_url": "https://foursquare.com/whatever",
+    "rss_url": "https://myrss.com/whatever",
+    "twitter_url": "https://twitter.com/whatever",
+    "facebook_url": "https://facebook.com/whatever",
+    "youtube_url": "https://youtube.com/whatever",
+    "instagram_url": "https://instagram.com/whatever",
+    "pinterest_url": "https://pinterest.com/whatever",
+    "linkedin_url": "https://linkedin.com/whatever",
+    "latitude": 40.7454277,
+    "longitude": -73.9877514,
+    "services_offered": [
+        "Some Service",
+        "Making you awesome",
+    ],
+    "hours_of_operation": [
+        {
+            "closes": "17:00:00",
+            "day_of_week": [
+                "Monday"
+            ],
+            "opens": "09:00:00",
+            "description": null
+        },
+        {
+            "closes": "20:00:00",
+            "day_of_week": [
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            ],
+            "opens": "09:00:00",
+            "description": null
+        },
+        {
+            "closes": null,
+            "day_of_week": [
+                "PublicHolidays"
+            ],
+            "opens": null,
+            "description": "Closed on Christmas"
+        }
+    ]
+}
+ *
+ */
 namespace BusinessProfileRender;
 
 defined( 'ABSPATH' ) || exit;
 
-
-$example_json_string = '{
-	"company_name": "nTest",
-	"description": "This is testing the loooong description",
-	"short_description": null,
-	"services_offered": [
-		"Being Awesome",
-		"Making you awesome",
-		"Dirty deeds done dirt cheap"
-	],
-	"contact_first_name": null,
-	"contact_last_name": null,
-	"contact_email": null,
-	"cell_number": "213-281-7668",
-	"fax_number": "Throw out all the fax machines",
-	"toll_free_number": null,
-	"work_number": [
-		"7183841638"
-	],
-	"address": "6 W 29th St",
-	"city": "New York",
-	"zip": "10001",
-	"state": "NY",
-	"country": "US",
-	"timezone": "America/New_York",
-	"longitude": -73.9877514,
-	"latitude": 40.7454277,
-	"hours_of_operation": [
-		{
-			"closes": "17:00:00",
-			"opens": "09:00:00",
-			"day_of_week": [
-			"Monday"
-		],
-			"description": null
-		},
-		{
-			"closes": "17:00:00",
-			"opens": "09:00:00",
-			"day_of_week": [
-			"Wednesday"
-		],
-			"description": null
-		},
-		{
-			"closes": null,
-			"opens": null,
-			"day_of_week": [
-			"PublicHolidays"
-		],
-			"description": "Closed on Christmas"
-		}
-	],
-	"images": {
-		"logo": "https://media-prod.apigateway.co/image/get/4d1fc154-05b1-43ef-a168-1700aa919e41",
-		"primary": "https://media-prod.apigateway.co/image/get/2fd2dbd2-8150-4c62-b623-e7284b93d20b"
-	},
-	"rss_url": null,
-	"twitter_url": null,
-	"foursquare_url": null,
-	"facebook_url": null,
-	"youtube_url": null,
-	"instagram_url": null,
-	"pinterest_url": null,
-	"linkedin_url": null
-}';
 
 /**
  * Class DataStorage
@@ -122,7 +136,7 @@ class DataStorage {
 	/**
 	 * @param string $property_name - name of the field in the business profile array (like "company_name")
 	 *
-	 * @return string|null - the value stored in the structured option data like ("Alphabet Inc.")
+	 * @return string|mixed|null - the value stored in the structured option data like ("Alphabet Inc.")
 	 */
 	public function get( $property_name ) {
 		if ( is_null( $this->business_profile_array ) ) {
