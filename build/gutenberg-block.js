55e4cef46dc53476fd74136d81f86514710bf529
Manish Yadav <msubachchanyadav@vendasta.com>
2024-07-17 18:15:02 +0530

    consolidate into a single function

--- build/gutenberg-block.js
+++ build/gutenberg-block.js
@@ -5,6 +5,70 @@
         { SelectControl: l } = wp.components,
         { useState: n, useEffect: s } = wp.element;
 
+    const parseUrl = (url) => {
+        try {
+            const cleanUrl = url.replace(/,+$/, '');
+            return new URL(cleanUrl);
+        } catch (e) {
+            return null;
+        }
+    };
+
+    const isImageUrl = (url) => {
+        const parsedUrl = parseUrl(url);
+        return parsedUrl && parsedUrl.hostname === 'media-prod.apigateway.co';
+    };
+
+    const getSocialIcon = (url) => {
+        const parsedUrl = parseUrl(url);
+        if (!parsedUrl) return null;
+        const { hostname } = parsedUrl;
+
+        switch (true) {
+            case hostname.includes("facebook.com"):
+                return "fab fa-facebook";
+            case hostname.includes("rss"):
+                return "fab fa-rss";
+            case hostname.includes("twitter.com"):
+                return "fab fa-twitter";
+            case hostname.includes("youtube.com"):
+                return "fab fa-youtube";
+            case hostname.includes("foursquare.com"):
+                return "fab fa-foursquare";
+            case hostname.includes("instagram.com"):
+                return "fab fa-instagram";
+            case hostname.includes("pinterest.com"):
+                return "fab fa-pinterest";
+            case hostname.includes("linkedin.com"):
+                return "fab fa-linkedin";
+            default:
+                return null;
+        }
+    };
+
+    const Preview = ({ url }) => {
+        const socialIcon = getSocialIcon(url);
+        return (
+            e.createElement("div", null,
+                isImageUrl(url) ?
+                    e.createElement("p", null,
+                        e.createElement("span", { className: "css-1imalal" }, "Preview:"),
+                        e.createElement("br", null),
+                        e.createElement("img", { src: url.replace(/,+$/, ''), alt: "Business Profile Image", style: { width: "100px", height: "100px" } })
+                    ) :
+                socialIcon ?
+                    e.createElement("p", null,
+                        e.createElement("span", { className: "css-1imalal" }, "Preview:"),
+                        e.createElement("br", null),
+                        e.createElement("a", { href: url, target: "_blank", rel: "noopener noreferrer" },
+                            e.createElement("i", { className: socialIcon })
+                        )
+                    ) :
+                    e.createElement("p", null, url)
+            )
+        );
+    };
+
     t("business-profile-render/bpr-block", {
         title: "Business Profile",
         icon: "admin-site",
@@ -23,49 +87,6 @@
                 c(t);
             }, []);
 
-            const isImageUrl = (url) => {
-                try {
-                    const cleanUrl = url.replace(/,+$/, '');
-                    const parsedUrl = new URL(cleanUrl);
-                    return parsedUrl.hostname === 'media-prod.apigateway.co';
-                } catch (e) {
-                    return false;
-                }
-            };
-
-            const getSocialIcon = (url) => {
-                let hostname;
-                try {
-                    const parsedUrl = new URL(url);
-                    hostname = parsedUrl.hostname;
-                } catch (_) {
-                    return null;
-                }
-
-                switch (true) {
-                    case hostname.includes("facebook.com"):
-                        return "fab fa-facebook";
-                    case hostname.includes("rss"):
-                        return "fab fa-rss";
-                    case hostname.includes("twitter.com"):
-                        return "fab fa-twitter";
-                    case hostname.includes("youtube.com"):
-                        return "fab fa-youtube";
-                    case hostname.includes("foursquare.com"):
-                        return "fab fa-foursquare";
-                    case hostname.includes("instagram.com"):
-                        return "fab fa-instagram";
-                    case hostname.includes("pinterest.com"):
-                        return "fab fa-pinterest";
-                    case hostname.includes("linkedin.com"):
-                        return "fab fa-linkedin";
-                    default:
-                        return null;
-                }
-            };
-
-            const socialIcon = getSocialIcon(i);
-
             return (
                 e.createElement("div", null,
                     e.createElement(l, {
@@ -74,87 +95,13 @@
                         options: r.map(e => ({ label: e.label, value: e.value })),
                         onChange: e => { o({ option: e }) }
                     }),
-                    e.createElement("div", null,
-                        isImageUrl(i) ?
-                            e.createElement("p", null,
-                                e.createElement("span", { className: "css-1imalal" }, "Preview:"),
-                                e.createElement("br", null),
-                                e.createElement("img", { src: i.replace(/,+$/, ''), alt: "Business Profile Image", style: { width: "100px", height: "100px" } })
-                            ) :
-                        socialIcon ?
-                            e.createElement("p", null,
-                                e.createElement("span", { className: "css-1imalal" }, "Preview:"),
-                                e.createElement("br", null),
-                                e.createElement("a", { href: i, target: "_blank", rel: "noopener noreferrer" },
-                                    e.createElement("i", { className: socialIcon })
-                                )
-                            ) :
-                            e.createElement("p", null, i)
-                    )
+                    e.createElement(Preview, { url: i })
                 )
             );
         },
         save: ({ attributes: t }) => {
             const { option: l } = t;
-
-            const isImageUrl = (url) => {
-                try {
-                    const cleanUrl = url.replace(/,+$/, '');
-                    const parsedUrl = new URL(cleanUrl);
-                    return parsedUrl.hostname === 'media-prod.apigateway.co';
-                } catch (e) {
-                    return false;
-                }
-            };
-
-            const getSocialIcon = (url) => {
-                let hostname;
-                try {
-                    const parsedUrl = new URL(url);
-                    hostname = parsedUrl.hostname;
-                } catch (_) {
-                    return null;
-                }
-
-                switch (true) {
-                    case hostname.includes("facebook.com"):
-                        return "fab fa-facebook";
-                    case hostname.includes("rss"):
-                        return "fab fa-rss";
-                    case hostname.includes("twitter.com"):
-                        return "fab fa-twitter";
-                    case hostname.includes("youtube.com"):
-                        return "fab fa-youtube";
-                    case hostname.includes("foursquare.com"):
-                        return "fab fa-foursquare";
-                    case hostname.includes("instagram.com"):
-                        return "fab fa-instagram";
-                    case hostname.includes("pinterest.com"):
-                        return "fab fa-pinterest";
-                    case hostname.includes("linkedin.com"):
-                        return "fab fa-linkedin";
-                    default:
-                        return null;
-                }
-            };
-
-            const socialIcon = getSocialIcon(l);
-
-            return (
-                e.createElement("div", null,
-                    isImageUrl(l) ?
-                        e.createElement("p", null,
-                            e.createElement("img", { src: l.replace(/,+$/, ''), alt: "Business Profile Image", style: { width: "100px", height: "auto" } })
-                        ) :
-                    socialIcon ?
-                        e.createElement("p", null,
-                            e.createElement("a", { href: l, target: "_blank", rel: "noopener noreferrer" },
-                                e.createElement("i", { className: socialIcon })
-                            )
-                        ) :
-                        e.createElement("p", null, l)
-                )
-            );
+            return e.createElement(Preview, { url: l });
         }
     });
 })();
